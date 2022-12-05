ea117c5e9f8275db41a365b42bf6a685bc2d786c
Sarkhan <sarkhan.eyn@gmail.com>
2022-12-05 21:35:57 +0100

    3 task

--- 3_homework.py
+++ 3_homework.py
@@ -19,15 +19,40 @@
 - [2, 3, 4, 5, 6] => [12, 15, 16];
 - [2, 3, 5, 6] => [12, 15]"""
 
-list_input = []
-dimension_input = int(input('Введите размерность списка: '))
-list_output = []
-dimension_output = dimension_input / 2
-if(dimension_input % 2 == 1):
-    dimension_output += 1
+# list_input = []
+# dimension_input = int(input('Введите размерность списка: '))
+# list_output = []
+# dimension_output = dimension_input / 2
+# if(dimension_input % 2 == 1):
+#     dimension_output += 1
 
-for i in range(dimension_input):
-    list_input.append(int(input(f'{i} позиция = ')))
-for i in range(int(dimension_output)):
-    list_output.append(int(list_input[i] * list_input[dimension_input - i - 1]))
-print(f'{list_input} => {list_output}')
+# for i in range(dimension_input):
+#     list_input.append(int(input(f'{i} позиция = ')))
+# for i in range(int(dimension_output)):
+#     list_output.append(int(list_input[i] * list_input[dimension_input - i - 1]))
+# print(f'{list_input} => {list_output}')
+
+
+
+"""Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+Пример:
+- [1.1, 1.2, 3.1, 5, 10.01] => 0.19"""
+
+# list_input = []
+# list_output = []
+# dimension = int(input('Введите размерность списка: '))
+
+# for i in range(dimension):
+#     list_input.append(float(input(f'{i} позиция = '))) 
+
+# for i in list_input:
+#     if (i // 1 != i):
+#         list_output.append(round(float(i - (i // 1)).real, 10)) 
+
+# max, min = list_output[0], list_output[0]
+# for i in list_output:
+#     if(min > i):
+#         min = i
+#     if(max < i):
+#         max = i
+# print(f'{list_input} => {max - min}')
