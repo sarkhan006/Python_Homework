bbc6571b9b1dfe6fa8cb51e15512baf87fb0b707
Sarkhan <sarkhan.eyn@gmail.com>
2022-12-05 21:39:21 +0100

    4 task

--- 3_homework.py
+++ 3_homework.py
@@ -56,3 +56,23 @@
 #     if(max < i):
 #         max = i
 # print(f'{list_input} => {max - min}')
+
+
+
+"""Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+Пример:
+- 45 -> 101101
+- 3 -> 11
+- 2 -> 10
+"""
+number = int(input('Введите число: '))
+binary = []
+count = 0
+
+while number != 0:
+    binary.append(int(number % 2))
+    number = int((number - binary[count]) // 2)
+    count += 1
+for i in range(count // 2):
+    binary[i], binary[count - 1 - i] = binary[count - 1 - i], binary[i]
+print(binary)
