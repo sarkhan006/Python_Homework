f6f09b92697fdd274af30991e389526224c2838d
Sarkhan <sarkhan.eyn@gmail.com>
2022-12-14 23:12:27 +0100

    2 task

--- 4_homework.py
+++ 4_homework.py
@@ -1,10 +1,28 @@
-# Вычислить число c заданной точностью d
+""" Вычислить число c заданной точностью d """
 
-d = float(input('Введите точность: '))
-number = float(input('Введите число: '))
-count = 0
+# d = float(input('Введите точность: '))
+# number = float(input('Введите число: '))
+# count = 0
 
-while d < 1:
-    count += 1
-    d *= 10
-print(f'%.{count}f' % number)
+# while d < 1:
+#     count += 1
+#     d *= 10
+# print(f'%.{count}f' % number)
+
+
+"""Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N."""
+
+# import sympy
+
+#output = {0}
+#count = 1
+
+#output.clear()
+#while number > 1:
+#    if number / sympy.prime(count) != number // sympy.prime(count):
+#        count += 1
+#    else:
+#        output.add(sympy.prime(count))
+#        number /= sympy.prime(count)
+#        count = 1       
+#print(output)
