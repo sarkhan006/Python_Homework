d33120773c3786bd93ae88af2b6c6a08ed2ec660
Sarkhan <sarkhan.eyn@gmail.com>
2022-12-14 23:14:05 +0100

    3 task

--- 4_homework.py
+++ 4_homework.py
@@ -26,3 +26,22 @@
 #        number /= sympy.prime(count)
 #        count = 1       
 #print(output)
+
+
+# Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N."""
+
+"""import sympy
+
+number = int(input('Введите число: '))
+output = {0}
+count = 1
+
+output.clear()
+while number > 1:
+    if number / sympy.prime(count) != number // sympy.prime(count):
+        count += 1
+    else:
+        output.add(sympy.prime(count))
+        number /= sympy.prime(count)
+        count = 1       
+print(output) """
